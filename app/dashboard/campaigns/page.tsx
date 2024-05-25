71d77f6e4af15bf5441b342946a97a173dcc2662
ramilbaldoz <rb@linkmedia.com>
2024-05-26 00:15:58 +0400

    update: added condition

--- app/dashboard/campaigns/page.tsx
+++ app/dashboard/campaigns/page.tsx
@@ -3,6 +3,32 @@ import { Card } from "@/components/ui/card"
 import { Button } from '@/components/ui/button';
 import PageTitle from '@/components/dashboard/PageTitle';
 
+type CampaignListProps = {
+  hasCampaigns: boolean;
+};
+
+const CampaignList: React.FC<CampaignListProps> = ({ hasCampaigns }) => {
+  if (hasCampaigns) {
+    return <li className="item">Show Campaigns</li>;
+  } else {
+    return (
+      <Card className="h-full p-10 flex flex-col justify-center items-center gap-4 bg-background">
+        <div className="w-full h-full flex items-center justify-center border border-dashed border-muted-foreground rounded-md">
+          <div className="w-[520px] flex flex-col items-center gap-4 text-center">
+            <h3 className="text-xl text-neutral-700 font-semibold">Welcome to Verthem! 👋🏼</h3>
+            <p className="text-muted-foreground">
+              Start crafting a visually appealing and high-converting page today. Our intuitive builder lets you drag, drop, and customize every element to match your marketing needs!
+            </p>
+            <Button className="flex gap-2 bg-verthem-900 hover:bg-verthem-700 hover:transition-all text-base text-slate-900 font-medium">
+              Start Building Now
+            </Button>
+          </div>
+        </div>
+      </Card>
+    );
+  }
+};
+
 export default function CampaignsPage() {
   const subText: string = "Display all related campaigns here.";
 
@@ -10,18 +36,8 @@ export default function CampaignsPage() {
     <section className="h-full flex flex-col gap-4">
       <PageTitle subText={subText} />
       
-      {/* TODO: move as components - display if no campaigns found */}
-      <Card className="h-full p-10 flex flex-col justify-center items-center gap-4 bg-background">
-        <div className="w-full h-full flex items-center justify-center border border-dashed border-muted-foreground rounded-md">
-          <div className="w-[520px] flex flex-col items-center gap-4 text-center">
-            <h3 className="text-xl text-neutral-700 font-semibold">Welcome to Verthem! 👋🏼</h3>
-            <p className="text-muted-foreground">Start crafting a visually appealing and high-converting page today. Our intuitive builder lets you drag, drop, and customize every element to match your marketing needs!</p>
-            <Button className="flex gap-2 bg-verthem-900 hover:bg-verthem-700 hover:transition-all text-base text-slate-900 font-medium">Start Building Now</Button>
-          </div>
-        </div>
-      </Card>
-
-      {/* TODO: Display campaigns here */}
+      {/* TODO: replace condition */}
+      <CampaignList hasCampaigns={false} />
 
     </section>
   );
