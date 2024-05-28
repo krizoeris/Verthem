a3ed94fd0494cab843e4335e37dc87eed4ff588d
Mark Valenzuela <mv@linkmedia.com>
2024-05-28 20:59:18 +0400

    fix: convert image to html

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,18 +1,11 @@
-import Image from "next/image";
+import VerthemBG from "@/components/auth/VerthemBg";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
       <div className="flex items-center justify-center py-12">{children}</div>
-      <div className="hidden bg-muted lg:block">
-        <Image
-          src="/images/workspace.jpg"
-          alt="Image"
-          width="1920"
-          height="1080"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
-      </div>
+
+      <VerthemBG />
     </div>
   );
 }
