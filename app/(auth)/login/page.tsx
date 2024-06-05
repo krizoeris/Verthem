0f59f2a7c7e250640d08fed0059df96be191ebc2
Mark Valenzuela <mv@linkmedia.com>
2024-06-05 20:45:16 +0400

    fix: login caption

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -9,9 +9,7 @@ export default function LoginPage() {
     <div className="mx-auto grid w-[350px] gap-6">
       <div className="grid gap-2 text-center">
         <h1 className="text-3xl font-bold">Login</h1>
-        <p className="text-balance text-muted-foreground">
-          Enter your email below to login to your account
-        </p>
+        <p className="text-balance text-muted-foreground">Get started now.</p>
       </div>
       <div className="grid gap-4">
         <LoginProvider provider="google" />
