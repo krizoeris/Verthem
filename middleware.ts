80c2a90c6d996e88a96447ef98c9fbd48dbbc4f6
Mark Valenzuela <mv@linkmedia.com>
2024-06-09 13:49:48 +0400

    fix: resolve conflict

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,30 @@
 import NextAuth from "next-auth";
+import { NextResponse, NextRequest } from "next/server";
 import { authConfig } from "@/auth/auth.config";
 
-export default NextAuth(authConfig).auth;
+const { auth } = NextAuth(authConfig);
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|images).*)"],
 };
+
+export default auth(async function middleware(req: NextRequest) {
+  const session = await auth();
+  const publicURL = ["/", "/login"];
+
+  if (req.nextUrl.pathname.startsWith("/_next")) {
+    return NextResponse.next();
+  }
+
+  if (!session && !publicURL.includes(req.nextUrl.pathname)) {
+    const URL = req.nextUrl.clone();
+    URL.pathname = "/login";
+    return NextResponse.redirect(URL);
+  }
+
+  if (session && req.nextUrl.pathname === "/login") {
+    const URL = req.nextUrl.clone();
+    URL.pathname = "/dashboard";
+    return NextResponse.redirect(URL);
+  }
+});
